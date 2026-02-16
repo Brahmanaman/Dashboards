a2d7d1fea123904636f0ee537a18be18abe21a8e
Aman Sharma <sharma9917375201@gmail.com>
2026-02-16 12:10:49 +0530

    added scrollToTop functionality

--- BlogWebsite/script.js
+++ BlogWebsite/script.js
@@ -1,4 +1,18 @@
 const mobileMenu = document.querySelector(".mobile-menu");
+const scrollbtn = document.querySelector(".scroll-top");
 mobileMenu.addEventListener("click", function () {
     document.querySelector(".menu").classList.toggle("show");
+})
+
+window.addEventListener("scroll", function () {
+    if (this.document.body.scrollTop > 100 || this.document.documentElement.scrollTop > 100) {
+        scrollbtn.style.display = "flex";
+    }
+    else {
+        scrollbtn.style.display = "none";
+    }
+})
+
+scrollbtn.addEventListener('click', function () {
+    document.documentElement.scrollTop = 0;
 })
